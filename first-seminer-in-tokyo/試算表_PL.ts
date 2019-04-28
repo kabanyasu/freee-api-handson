8339734d53b206832ba583c17037a339b98bfedd
kabanyasu <daisuke.0108birth@gmail.com>
2019-04-28 20:43:34 +0900

    add comment

--- first-seminer-in-tokyo/試算表_PL.ts
+++ first-seminer-in-tokyo/試算表_PL.ts
@@ -14,32 +14,36 @@ function getPl() {
   //アクセストークンを取得
   var freeeApp = getService();
   var accessToken = freeeApp.getAccessToken();
-  
+
   //URLで情報の参照先URLとパラメータ（条件）指定を行います。
-  var url = "https://api.freee.co.jp/api/1/reports/trial_pl"; //情報の取得先
-  var url = url + "?company_id=" + PARAMS[0];
-  var url = url + "&fiscal_year=" + PARAMS[1];
-  var url = url + "&start_month=" + PARAMS[2];
-  var url = url + "&end_month=" + PARAMS[3];
-  
+  var keyUrl = "https://api.freee.co.jp/api/1/reports/trial_pl?"; //情報の取得先
+  let companyId = PARAMS[0];
+  let fiscalYear = PARAMS[1];
+  let startMonth = PARAMS[2];
+  let endMonth = PARAMS[3];
+  var url =
+    keyUrl +
+    `company_id=${companyId}&fiscal_year=${fiscalYear}&start_month=${startMonth}&end_month=${endMonth}`;
+
   //HTTPリクエストを送る際のオプションを指定します。
   var options = {
-    "method": "get",
-    "headers": {
-      "Authorization": "Bearer " + accessToken
+    method: "get",
+    headers: {
+      Authorization: "Bearer " + accessToken
     }
   };
-  
+
   //freeeからデータを取得し、JSON形式に変換した後、残高情報のある部分を抜き出します。
   var res = UrlFetchApp.fetch(url, options).getContentText();
   var res = JSON.parse(res);
   var resPl = res.trial_pl.balances;
-  
+
   //科目ごとに、取得したPLの内容を格納していきます。
   var rowData = [];
-  for (var i in resPl){
-    if(!resPl[i].total_line){
+  for (var i in resPl) {
+    if (!resPl[i].total_line) {
       rowData.push([
+        /*[勘定科目ID, 勘定科目名, 改装レベル, 開始残高, 借方金額, 貸方金額, 期末残高] */
         resPl[i].account_item_id,
         resPl[i].account_item_name,
         resPl[i].hierarchy_level,
@@ -48,8 +52,9 @@ function getPl() {
         resPl[i].credit_amount,
         resPl[i].closing_balance
       ]);
-    }else{
-       rowData.push([
+    } else {
+      rowData.push([
+        /*[勘定科目ID, 科目区分, 改装レベル, 開始残高, 借方金額, 貸方金額, 期末残高] */
         resPl[i].account_category_id,
         resPl[i].account_category_name,
         resPl[i].hierarchy_level,
@@ -57,12 +62,11 @@ function getPl() {
         resPl[i].debit_amount,
         resPl[i].credit_amount,
         resPl[i].closing_balance
-       ]);
-    };
-  };
-  
+      ]);
+    }
+  }
+
   //取得したデータをスプレッドシートに転記しましょう。
   PL_SHEET.getRange(2, rowData[0].length).clear();
   PL_SHEET.getRange(2, 1, rowData.length, rowData[0].length).setValues(rowData);
-  
 }
