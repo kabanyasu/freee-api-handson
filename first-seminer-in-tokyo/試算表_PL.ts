e6a70364efaa27d8ee61b3710d420ab6395c1f91
kabanyasu <daisuke.0108birth@gmail.com>
2019-05-02 13:20:06 +0900

    change readme

--- first-seminer-in-tokyo/試算表_PL.ts
+++ first-seminer-in-tokyo/試算表_PL.ts
@@ -14,31 +14,31 @@ function getPl() {
   //アクセストークンを取得
   var freeeApp = getService();
   var accessToken = freeeApp.getAccessToken();
-  
+
   //URLで情報の参照先URLとパラメータ（条件）指定を行います。
   var url = "https://api.freee.co.jp/api/1/reports/trial_pl"; //情報の取得先
   url += "?company_id=" + PARAMS[0];
   url += "&fiscal_year=" + PARAMS[1];
   url += "&start_month=" + PARAMS[2];
   url += "&end_month=" + PARAMS[3];
-  
+
   //HTTPリクエストを送る際のオプションを指定します。
   var options = {
-    "method": "get",
-    "headers": {
-      "Authorization": "Bearer " + accessToken
+    method: "get",
+    headers: {
+      Authorization: "Bearer " + accessToken
     }
   };
-  
+
   //freeeからデータを取得し、JSON形式に変換した後、残高情報のある部分を抜き出します。
   var res = UrlFetchApp.fetch(url, options).getContentText();
   var res = JSON.parse(res);
   var resPl = res.trial_pl.balances;
-  
+
   //科目ごとに、取得したPLの内容を格納していきます。
   var rowData = [];
-  for (var i in resPl){
-    if(!resPl[i].total_line){
+  for (var i in resPl) {
+    if (!resPl[i].total_line) {
       rowData.push([
         resPl[i].account_item_id,
         resPl[i].account_item_name,
@@ -48,8 +48,8 @@ function getPl() {
         resPl[i].credit_amount,
         resPl[i].closing_balance
       ]);
-    }else{
-       rowData.push([
+    } else {
+      rowData.push([
         resPl[i].account_category_id,
         resPl[i].account_category_name,
         resPl[i].hierarchy_level,
@@ -57,12 +57,11 @@ function getPl() {
         resPl[i].debit_amount,
         resPl[i].credit_amount,
         resPl[i].closing_balance
-       ]);
-    };
-  };
-  
+      ]);
+    }
+  }
+
   //取得したデータをスプレッドシートに転記しましょう。
   PL_SHEET.getRange(2, rowData[0].length).clear();
   PL_SHEET.getRange(2, 1, rowData.length, rowData[0].length).setValues(rowData);
-  
 }
